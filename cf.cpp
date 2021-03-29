c6a41e5f669423b186e0ba16e1572207382d11a9
Inderpreet02 <inder192002@gmail.com>
2021-03-29 19:34:04 +0530

    Nothing special

--- cf.cpp
+++ cf.cpp
@@ -2,42 +2,55 @@
 
 using namespace std;
 
+
+#define ll              long long
+#define pb              push_back
+#define mp              make_pair
+#define ff              first
+#define ss              second
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define vt(type)        vector<type>
+
+void solve(){
+    
+    // vector<pair<int, int>> vpi;
+    // vector <int> vi;
+    // set <char> sc;
+    // set <int> si;
+    // 97(a) - 65(A) = 32  122(z)
+    string str, word, tempstr;
+
+    bool flag = true;
+
+    double ans1=0, c, d, ans2=0, sol=0;
+    ll n;cin>>n;
+
+    cin >> d;
+    // cin >> c;
+
+    // cin >> str;
+
+    // cout << typeid(d).name() << d << " " << (1.2 == (int)1.99);
+
+    
+
+    
+}
+
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     
-    int n;
+    ll t=1;
 
-    cin >> n;
+    cin >> t;
 
-    while(n--){
-        int a;
-
-        cin >> a;
-        int arr1[a];
+    while(t--){
         
+        solve();
 
-        for(int i=0; i<a; i++){
-            int temp; cin >> temp;
-            arr1[i] = temp;
-        }
-
-
-        for(int k=0; k<a; k++){
-            int sum = 0;
-            for(int j=0; j<a; j++){
-                if(k!=j){
-                    sum+=arr1[j];
-                }
-            }
-            cout << sum << " ";
-        }
-
-
-        cout << endl;
+        cout << "\n";
     }
-
-    
 
     return 0;
 }
